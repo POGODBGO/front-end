2b3194f01b5d292bfd75ffea36a1ea7ebb150172
jihyye <tlsjh082@naver.com>
2018-12-12 03:30:05 +0900

    통신 1차

--- app/src/main/java/com/team/dbgo/SplashActivity.kt
+++ app/src/main/java/com/team/dbgo/SplashActivity.kt
@@ -15,7 +15,7 @@ class SplashActivity : AppCompatActivity() {
         val handler = Handler()
         handler.postDelayed({
             startActivity(Intent(applicationContext, MainActivity::class.java))
-            //startActivity(Intent(applicationContext, MypokemonActivity::class.java))
+            //startActivity(Intent(applicationContext, LoginActivity::class.java))
             finish()
         }, 3000)
     }
